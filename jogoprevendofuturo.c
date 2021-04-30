9947565bdd2b74da81b798171f953ea1c3a0d840
Raphaela Oliveira <80285223+raphaoliveiranogueira@users.noreply.github.com>
2021-04-30 15:25:16 -0300

    Add files via upload

---
+++ jogoprevendofuturo.c
@@ -0,0 +1,37 @@
+#include <stdio.h>  /*bibliotecas padrões em linguagem C*/
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+
+int main(void) {
+
+
+char* banco [3];  /* variáveis char para armazenar as respostas*/
+banco[0]= "sim";
+banco[1]= "nao";
+banco[2]= "talvez";
+char * pergunta;
+int resposta; 
+int i; 
+
+printf("\n\nPergunte-me qualquer coisa, responderei com Sim, Não ou Talvez\n\n");
+scanf("%s", pergunta);
+
+srand(time(NULL)); /*função que só funciona com a biblioteca time.h*/
+for (i = 0; i < 11; i++) {
+resposta = rand() % 3;
+
+printf("%s", banco[resposta]); /*comparamos o banco com as respostas para dar a resposta aleatória ao usuário*/
+
+return (0);
+}
+}
+
+
+
+
+
+
+
